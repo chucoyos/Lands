609dc77383e9472290c824049de863b7ea3b9427
chucoyos <30364462+chucoyos@users.noreply.github.com>
2019-01-10 03:33:01 -0600

    Clase

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,40 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Lands.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemembered { get; set; }
+        #endregion
+
+        #region Constructores
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+         }
+
+        private void Login()
+        {
+            
+        }
+        #endregion
+    }
+}
